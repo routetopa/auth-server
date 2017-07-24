eb2477a2646f2ac7f6d64b821743c55c5decc6a1
Luca Vicidomini <lvicido@libero.it>
2017-07-24 11:59:41 +0200

    Fixed typo

--- ajax.php
+++ ajax.php
@@ -4,7 +4,7 @@ $path_extra = (dirname(dirname(__FILE__)));
 $path = ini_get('include_path');
 $path = $path_extra . PATH_SEPARATOR . $path;
 $path .= PATH_SEPARATOR . dirname(__FILE__) . DIRECTORY_SEPARATOR . 'inc';
-$path .= PATH_SEPARATOR . $path_extra . DIRECTORY_SEPARATOR . 'ow_libraries' . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'smarty' .DIRECTORY_SEPARATOR . 'smarty' . D$
+$path .= PATH_SEPARATOR . $path_extra . DIRECTORY_SEPARATOR . 'ow_libraries' . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'smarty' .DIRECTORY_SEPARATOR . 'smarty' . DIRECTORY_SEPARATOR . 'libs';
 
 ini_set('include_path', $path);
 
@@ -34,7 +34,7 @@ $password = $_POST['password'];
 
 list($errors, $user) = db_authUserByEmail($username, $password);
 
-$message = $errors ? 'Authentication failed' : 'Authentication successfull';
+$message = $errors ? 'Authentication failed' : 'Authentication successf';
 
 $result = array(
         'result' => $user ? true : false,
